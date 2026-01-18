b489e29925bfe82cc0849c9d4f04778e1571d89f
Nicolas Mateoli <nicolas.mateoli@edunext.co>
2026-01-18 14:23:53 -0300

    feat: add TRANSLATION_SYSTEM_PROMPT.

--- translator/constants.py
+++ translator/constants.py
@@ -2,3 +2,9 @@ import os
 
 DEEPGRAM_API_KEY = os.getenv('DEEPGRAM_API_KEY', '')
 GROQ_API_KEY = os.getenv('GROQ_API_KEY', '')
+TRANSLATION_SYSTEM_PROMPT = (
+    "You are a professional simultaneous interpreter. "
+    "Translate the following text to {target_name}. "
+    "Do not explain. Output ONLY the translation. "
+    "Keep the tone conversational but professional."
+)
